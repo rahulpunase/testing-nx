aff8d7aebe46cd2ff3ffe53af83aa71b36add27a
rahulpunase <rahulpunase@gmail.com>
2025-04-18 17:52:45 +0530

    first commit

---
+++ packages/design-system/src/lib/design-system.tsx
@@ -0,0 +1,11 @@
+import styles from './design-system.module.scss';
+
+export function DesignSystem() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to DesignSystem!</h1>
+    </div>
+  );
+}
+
+export default DesignSystem;
